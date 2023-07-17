95da4c7c422f7acbfe8f7ed570e35f978e21455d
Michal Radomski <michalra@op.pl>
2023-07-18 01:52:16 +0200

    Advanced React-Redux course - commit 05

--- 01 Advanced React/1 Testing/src/App.test.tsx
+++ 01 Advanced React/1 Testing/src/App.test.tsx
@@ -1,11 +1,16 @@
-// import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
 
 test("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <React.Fragment>
+      <App />
+    </React.Fragment>,
+    div
+  );
   expect(div.innerHTML).toContain("Hi there!");
   ReactDOM.unmountComponentAtNode(div);
 });
