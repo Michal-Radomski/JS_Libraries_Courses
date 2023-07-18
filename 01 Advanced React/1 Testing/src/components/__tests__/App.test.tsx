a5c3d058c6678aeef88b1c9f7c6e2caa5e383565
Michal Radomski <michalra@op.pl>
2023-07-19 01:50:29 +0200

    Advanced React-Redux course - commit 10

--- 01 Advanced React/1 Testing/src/components/__tests__/App.test.tsx
+++ 01 Advanced React/1 Testing/src/components/__tests__/App.test.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 
 import App from "../App";
+import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 
 //* First test
 // test("renders without crashing", () => {
@@ -16,16 +19,25 @@ import App from "../App";
 //   ReactDOM.unmountComponentAtNode(div);
 // });
 
+//* Test 2
+// it("shows a comment box", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(
+//     <React.Fragment>
+//       <App />
+//     </React.Fragment>,
+//     div
+//   );
+//   console.log(div.innerHTML);
+//   expect(div.innerHTML).toContain("Comment Box");
+//   expect(div.innerHTML).toContain("Comment List");
+//   expect(div.innerHTML).toBeTruthy();
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+//* Test 3
 it("shows a comment box", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(
-    <React.Fragment>
-      <App />
-    </React.Fragment>,
-    div
-  );
-  // console.log(div.innerHTML);
-  expect(div.innerHTML).toContain("Comment Box");
-  expect(div.innerHTML).toContain("Comment List");
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
