cfb840c6ccc170a39e3d6b45439b1faef3c9b384
Michal Radomski <michalra@op.pl>
2023-07-20 02:37:52 +0200

    Advanced React-Redux course - commit 15

--- 01 Advanced React/1 Testing/src/components/CommentBox.tsx
+++ 01 Advanced React/1 Testing/src/components/CommentBox.tsx
@@ -2,21 +2,39 @@ import React from "react";
 
 //* V1
 // const CommentBox = (): JSX.Element => {
+//   const [state, setState] = React.useState<{ comment: string }>({ comment: "" });
+
+//   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+//     setState({ comment: event.target.value });
+//   };
+
 //   return (
 //     <React.Fragment>
-//       <div>Comment Box</div>
+//       <form>
+//         <h4>Add a Comment</h4>
+//         <textarea value={state.comment} onChange={handleChange} />
+//         <div>
+//           <button>Submit Comment</button>
+//         </div>
+//       </form>
 //     </React.Fragment>
 //   );
 // };
 
 //* V2
 class CommentBox extends React.Component {
+  state = { comment: "" };
+
+  handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    this.setState({ comment: event.target.value });
+  };
+
   render() {
     return (
       <React.Fragment>
         <form>
           <h4>Add a Comment</h4>
-          <textarea />
+          <textarea value={this.state.comment} onChange={this.handleChange} />
           <div>
             <button>Submit Comment</button>
           </div>
