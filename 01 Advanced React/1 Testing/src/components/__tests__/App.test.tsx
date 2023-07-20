fb03862078ebf1dfb57a0c54adc99a8240074558
Michal Radomski <michalra@op.pl>
2023-07-20 02:20:55 +0200

    Advanced React-Redux course - commit 13

--- 01 Advanced React/1 Testing/src/components/__tests__/App.test.tsx
+++ 01 Advanced React/1 Testing/src/components/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 // import ReactDOM from "react-dom";
-import { shallow } from "enzyme";
+import { ShallowWrapper, shallow } from "enzyme";
 
 import App from "components/App";
 import CommentBox from "components/CommentBox";
@@ -36,12 +36,18 @@ import CommentList from "components/CommentList";
 // });
 
 //* Test 3
+
+let wrapped: ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
 it("shows a comment box", () => {
-  const wrapped = shallow(<App />);
+  // const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
-  const wrapped = shallow(<App />);
+  // const wrapped = shallow(<App />);
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
