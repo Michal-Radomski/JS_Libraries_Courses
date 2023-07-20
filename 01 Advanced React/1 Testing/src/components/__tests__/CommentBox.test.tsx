b634a59989ad571f1f4b7f8d0be6b9b51d726450
Michal Radomski <michalra@op.pl>
2023-07-20 03:10:40 +0200

    Advanced React-Redux course - commit 18

--- 01 Advanced React/1 Testing/src/components/__tests__/CommentBox.test.tsx
+++ 01 Advanced React/1 Testing/src/components/__tests__/CommentBox.test.tsx
@@ -1,10 +1,19 @@
-import { mount } from "enzyme";
+import { ReactWrapper, mount } from "enzyme";
 
 import CommentBox from "components/CommentBox";
 
 //* Test 4
+let wrapped: ReactWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
 it("has a text area and a button", () => {
-  const wrapped = mount(<CommentBox />);
+  // const wrapped = mount(<CommentBox />);
   // console.log(wrapped.find("textarea").length);
   // console.log(wrapped.find("button").length);
   expect(wrapped.find("textarea").length).toEqual(1);
