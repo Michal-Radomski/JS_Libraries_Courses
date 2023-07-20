31f154ced38db045a3e079c0af67eb33cdcb37a1
Michal Radomski <michalra@op.pl>
2023-07-20 02:25:32 +0200

    Advanced React-Redux course - commit 14

--- 01 Advanced React/1 Testing/src/components/CommentBox.tsx
+++ 01 Advanced React/1 Testing/src/components/CommentBox.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 
-const CommentBox = (): JSX.Element => {
-  return (
-    <React.Fragment>
-      <div>Comment Box</div>
-    </React.Fragment>
-  );
-};
+//* V1
+// const CommentBox = (): JSX.Element => {
+//   return (
+//     <React.Fragment>
+//       <div>Comment Box</div>
+//     </React.Fragment>
+//   );
+// };
+
+//* V2
+class CommentBox extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <form>
+          <h4>Add a Comment</h4>
+          <textarea />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
 
 export default CommentBox;
