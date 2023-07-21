cd8526fb0970cc11d49de55156c19015fd7d71f9
Michal Radomski <michalra@op.pl>
2023-07-21 02:42:03 +0200

    Advanced React-Redux course - commit 26

--- 01 Advanced React/1 Testing/src/index.tsx
+++ 01 Advanced React/1 Testing/src/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//* Redux
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
+
 import App from "components/App";
+import rootReducer from "reducers/index";
+
+const store = createStore(rootReducer, {});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root") as HTMLElement
 );
