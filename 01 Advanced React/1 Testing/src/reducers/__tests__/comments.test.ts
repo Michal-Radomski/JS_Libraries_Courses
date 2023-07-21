9deeebacb5f066bc678d550774ef5948f1aa669c
Michal Radomski <michalra@op.pl>
2023-07-22 01:15:08 +0200

    Advanced React-Redux course - commit 32

--- 01 Advanced React/1 Testing/src/reducers/__tests__/comments.test.ts
+++ 01 Advanced React/1 Testing/src/reducers/__tests__/comments.test.ts
@@ -1,6 +1,7 @@
 import commentsReducers from "reducers/comments";
 import { SAVE_COMMENT } from "actions/types";
 
+//* Test 7
 it("handles actions of type SAVE_COMMENT", () => {
   const action = {
     type: SAVE_COMMENT,
@@ -8,4 +9,10 @@ it("handles actions of type SAVE_COMMENT", () => {
   };
   const newState = commentsReducers([], action);
   expect(newState).toEqual(["New Comment"]);
+});
+
+//* Test 8
+it("handles actions unknown type", () => {
+  const newState = commentsReducers([], {});
+  expect(newState).toEqual([]);
 });
