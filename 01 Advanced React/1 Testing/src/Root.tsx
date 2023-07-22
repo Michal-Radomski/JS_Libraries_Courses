e3eea49662657165ba80958008b4a9015f2c2144
Michal Radomski <michalra@op.pl>
2023-07-22 21:29:41 +0200

    Advanced React-Redux course - commit 38

--- 01 Advanced React/1 Testing/src/Root.tsx
+++ 01 Advanced React/1 Testing/src/Root.tsx
@@ -2,16 +2,19 @@ import React from "react";
 
 //* Redux
 import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import reduxPromise from "redux-promise";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 import rootReducer from "reducers/index";
 
 // const store = createStore(rootReducer, {});
 
 const Root = ({ children, initialState = {} }: { children: JSX.Element; initialState?: RootState }): JSX.Element => {
+  const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(reduxPromise)));
   return (
     <React.Fragment>
-      <Provider store={createStore(rootReducer, initialState)}>{children}</Provider>
+      <Provider store={store}>{children}</Provider>
     </React.Fragment>
   );
 };
