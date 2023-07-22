092006f5f0b42da39c07ed1d0439b4341c926aba
Michal Radomski <michalra@op.pl>
2023-07-23 00:44:01 +0200

    Advanced React-Redux course - commit 50

--- 01 Advanced React/2 HOC/src/components/App.tsx
+++ 01 Advanced React/2 HOC/src/components/App.tsx
@@ -5,13 +5,14 @@ import { connect } from "react-redux";
 import "../App.scss";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
+import * as actions from "actions";
 
-class App extends React.Component<{ auth: boolean }, {}> {
+class App extends React.Component<{ auth: boolean; changeAuth: (arg0: boolean) => void }, {}> {
   renderButton(): JSX.Element {
     if (this.props.auth) {
-      return <button>Sign Out</button>;
+      return <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>;
     } else {
-      return <button>Sign In</button>;
+      return <button onClick={() => this.props.changeAuth(true)}>Sign In</button>;
     }
   }
 
@@ -46,4 +47,4 @@ const mapStateToProps = (state: RootState) => {
   return { auth: state.auth };
 };
 
-export default connect(mapStateToProps, null)(App);
+export default connect(mapStateToProps, actions)(App);
