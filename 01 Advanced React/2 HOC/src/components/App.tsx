0afd2d60e267860ab0f69cc55c65f3eae2f90c1b
Michal Radomski <michalra@op.pl>
2023-07-23 00:25:51 +0200

    Advanced React-Redux course - commit 48

--- 01 Advanced React/2 HOC/src/components/App.tsx
+++ 01 Advanced React/2 HOC/src/components/App.tsx
@@ -1,19 +1,40 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 
 import "../App.scss";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
-function App(): JSX.Element {
-  return (
-    <React.Fragment>
-      <div>
-        <Route path="/post" component={CommentBox} />
-        <Route path="/" exact={true} component={CommentList} />
-      </div>
-    </React.Fragment>
-  );
+class App extends React.Component {
+  renderButton(): JSX.Element {
+    return <button></button>;
+  }
+
+  renderHeader(): JSX.Element {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          {this.renderHeader()}
+          <Route path="/post" component={CommentBox} />
+          <Route path="/" exact={true} component={CommentList} />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
