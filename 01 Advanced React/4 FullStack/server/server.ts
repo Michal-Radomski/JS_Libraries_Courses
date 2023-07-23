5f822a0f7a5281903555b3dd12a3da499b08b725
Michal Radomski <michalra@op.pl>
2023-07-23 23:59:27 +0200

    Advanced React-Redux course - commit 61

---
+++ 01 Advanced React/4 FullStack/server/server.ts
@@ -0,0 +1,47 @@
+import http from "http";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+import express, { Express, Request, Response } from "express";
+// import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+
+// The server
+const app: Express = express();
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("combined"));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy: false,
+  })
+);
+
+//Route middleware
+
+// Mongo DB
+
+// Test route
+app.get("/", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+});
+
+// Port
+const port = (process.env.PORT || 5000) as number;
+
+const server = http.createServer(app);
+server.listen({ port: port }, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+  // For testing only
+  console.log("Current Time:", new Date().toLocaleTimeString());
+});
