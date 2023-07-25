8edd3a17ecc4ab4378c9d75dd6c09c7a900f05a7
Michal Radomski <michalra@op.pl>
2023-07-26 01:14:18 +0200

    client - initial commit3

--- 01 Advanced React/4 FullStack/client/src/components/App.tsx
+++ 01 Advanced React/4 FullStack/client/src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./App.scss";
+import "../styles/App.scss";
 
 const App = (): JSX.Element => {
   return <React.Fragment>App</React.Fragment>;
---
+++ 01 Advanced React/4 FullStack/client/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div></div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Header;
