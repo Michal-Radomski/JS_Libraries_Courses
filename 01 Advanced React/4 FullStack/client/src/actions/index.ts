a76e15b014cd8631dbaf3192e10a4c3286e07f65
Michal Radomski <michalra@op.pl>
2023-07-27 01:32:40 +0200

    Advanced React-Redux course - commit 85

--- 01 Advanced React/4 FullStack/client/src/actions/index.ts
+++ 01 Advanced React/4 FullStack/client/src/actions/index.ts
@@ -3,8 +3,14 @@ import axios from "axios";
 import { AUTH_USER } from "./types";
 
 export const signup = (formProps: { email: string; password: string }) => async (dispatch: Dispatch) => {
-  console.log("dispatch:", dispatch);
-
-  const res = await axios.post("http://localhost:5000/api/signup", formProps);
-  console.log(res);
+  // console.log("dispatch:", dispatch);
+  try {
+    const response = await axios.post("http://localhost:5000/api/signup", formProps);
+    // console.log({response});
+    await dispatch({ type: AUTH_USER, payload: response.data.token });
+    localStorage.setItem("token", response.data.token);
+  } catch (error) {
+    console.log({ error });
+    // dispatch({ type: AUTH_ERROR, payload: 'Email in use' });
+  }
 };
