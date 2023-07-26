0556ae195c96ef32c61e6f8b87787025a61c1f06
Michal Radomski <michalra@op.pl>
2023-07-26 02:16:13 +0200

    Advanced React-Redux course - commit 81

--- 01 Advanced React/4 FullStack/client/src/components/auth/SignUp.tsx
+++ 01 Advanced React/4 FullStack/client/src/components/auth/SignUp.tsx
@@ -1,20 +1,42 @@
 import React from "react";
+import { reduxForm, Field } from "redux-form";
+// import { compose } from "redux";
+// import { connect } from "react-redux";
+import { History } from "history";
 
-class SignUp extends React.Component {
+interface Props {
+  history: History;
+  errorMessage: string;
+  handleSubmit: any;
+  signup: (arg0: any, arg1: () => void) => void;
+}
+
+class SignUp extends React.Component<Props, RootState> {
+  onSubmit = (formProps: any) => {
+    this.props.signup(formProps, () => {
+      this.props.history.push("/feature");
+    });
+  };
+
   render() {
+    const { handleSubmit } = this.props;
     return (
       <React.Fragment>
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
           <fieldset>
             <label>Email</label>
+            <Field name="email" type="text" component="input" autoComplete="none" />
           </fieldset>
           <fieldset>
             <label>Password</label>
+            <Field name="password" type="password" component="input" autoComplete="none" />
           </fieldset>
+          <div>{this.props.errorMessage}</div>
+          <button>Sign Up!</button>
         </form>
       </React.Fragment>
     );
   }
 }
 
-export default SignUp;
+export default reduxForm({ form: "signup" })(SignUp as any);
