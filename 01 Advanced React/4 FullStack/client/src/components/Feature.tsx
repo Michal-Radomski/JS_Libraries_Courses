feb9f59167d5dd1658f16a8d915b9975e3267125
Michal Radomski <michalra@op.pl>
2023-07-27 02:50:28 +0200

    Advanced React-Redux course - commit 90

--- 01 Advanced React/4 FullStack/client/src/components/Feature.tsx
+++ 01 Advanced React/4 FullStack/client/src/components/Feature.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import RequireAuth from "./RequireAuth";
+
 class Feature extends React.Component {
   render() {
     return <div>This is the feature!</div>;
   }
 }
 
-export default Feature;
+export default RequireAuth(Feature);
