b84dfeff2171ee79c835d3fa11e63f1e1e808b19
Michal Radomski <michalra@op.pl>
2023-07-29 03:03:24 +0200

    Advenced Node Course - commit 01

--- 02 Advanced Node/1 Node/loop.ts
+++ 02 Advanced Node/1 Node/loop.ts
@@ -1,10 +1,17 @@
 // ts-node myFile.ts
 
-// @ts-ignore
+const pendingTimers = [];
+const pendingOSTasks = [];
+const pendingOperations = [];
+
+// @ts-ignore -> New timers, tasks, operations are recorded from myFile.ts running
 myFile.runContents();
 
 function shouldContinue(): boolean {
-  return true || false;
+  // Check one: Any pending setTimeout, setInterval, setImmediate?
+  // Check two: Any pending OS tasks? (Like server listening to port)
+  // Check three: Any pending long running operations? (Like fs module)
+  return pendingTimers.length || pendingOSTasks.length || pendingOperations.length ? true : false;
 }
 
 // Entire body executes in one "tick"
