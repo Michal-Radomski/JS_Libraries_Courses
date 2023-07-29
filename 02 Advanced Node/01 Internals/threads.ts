94e0729cde690cca39c47d42995d082f3457450b
Michal Radomski <michalra@op.pl>
2023-07-29 15:06:37 +0200

    Advenced Node Course - commit 05

--- 02 Advanced Node/01 Internals/threads.ts
+++ 02 Advanced Node/01 Internals/threads.ts
@@ -1,8 +1,13 @@
 import crypto from "crypto";
 
 const start = Date.now();
-crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", (_err: Error | null, _derivedKey: Buffer) => {
   console.log("1:", Date.now() - start);
+  // const key = _derivedKey;
+  // console.log({ key });
+  // console.log(key.toString("base64"));
+  // console.log(key.toString("hex"));
+  // console.log(key.toString());
 });
 
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
