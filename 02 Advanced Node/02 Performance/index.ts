e68be6220654cf61f3b7b1ab232c3d5edec0dbc5
Michal Radomski <michalra@op.pl>
2023-07-30 00:00:58 +0200

    Advenced Node Course - commit 11

--- 02 Advanced Node/02 Performance/index.ts
+++ 02 Advanced Node/02 Performance/index.ts
@@ -1,25 +1,36 @@
 import http from "http";
+import cluster from "cluster";
 import express, { Express, Request, Response } from "express";
 
-// The server
-const app: Express = express();
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.ts to be executed *again* bu in child mode
+  cluster.fork();
+} else {
+  // I'm as child
 
-function doWork(duration: number) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+  // The server
+  const app: Express = express();
+
+  function doWork(duration: number) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {
+      //* do nothing
+    }
+  }
+
+  app.get("/", (req: Request, res: Response) => {
+    doWork(5000);
+    console.log("req.ip:", req.ip);
+    res.send("Hi there!");
+  });
+
+  const port = 3000;
+
+  const server = http.createServer(app);
+  server.listen({ port: port }, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+    // For testing only
+    console.log("Current Time:", new Date().toLocaleTimeString());
+  });
 }
-
-app.get("/", (req: Request, res: Response) => {
-  doWork(5000);
-  console.log("req.ip:", req.ip);
-  res.send("Hi there!");
-});
-
-const port = 3000;
-
-const server = http.createServer(app);
-server.listen({ port: port }, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
-  // For testing only
-  console.log("Current Time:", new Date().toLocaleTimeString());
-});
