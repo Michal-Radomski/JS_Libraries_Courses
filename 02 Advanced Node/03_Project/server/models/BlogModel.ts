080bb5a92233265e26a23e859588cd25d01b8053
Michal Radomski <michalra@op.pl>
2023-07-30 19:15:50 +0200

    Advenced Node Course - FullStack project setup - initial commit3

---
+++ 02 Advanced Node/03_Project/server/models/BlogModel.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IBlogModel extends Document {
+  title: String;
+  content: String;
+  createdAt: Date;
+  _user: Schema.Types.ObjectId;
+}
+
+const blogSchema: Schema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, minlength: [3, "Min displayName length is 3 characters"] },
+    content: { type: String, required: true, minlength: [3, "Min displayName length is 3 characters"] },
+    createdAt: { type: Date, default: Date.now },
+    _user: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IBlogModel>("Blog", blogSchema);
