c18e5af9d7433612b21b3af384c5c7a7812908b0
Michal Radomski <michalra@op.pl>
2023-08-06 01:46:00 +0200

    Advenced Node Course - commit 40

--- 02 Advanced Node/03_Project/server/tests/header.test.ts
+++ 02 Advanced Node/03_Project/server/tests/header.test.ts
@@ -1,15 +1,26 @@
-import puppeteer from "puppeteer";
+import puppeteer, { Browser, Page } from "puppeteer";
 
 // test("Adds two numbers", () => {
 //   const sum = 1 + 2;
 //   expect(sum).toEqual(3);
 // });
 
-test("We can launch a browser", async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+let page: Page;
+let browser: Browser;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({ headless: false });
+  page = await browser.newPage();
   // console.log("page", page);
   await page.goto("http://localhost:3000");
+});
+
+afterEach(async () => {
+  await page.close();
+  await browser.close();
+});
+
+test("We can launch a browser", async () => {
   const text = await page.$eval("a.brand-logo", (el: HTMLElement) => el.innerHTML);
   // console.log("text:", text);
   expect(text).toEqual("Blogster");
