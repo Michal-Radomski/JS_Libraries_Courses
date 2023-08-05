524ee747aef710690a03cc1169ca85df7a73dfbe
Michal Radomski <michalra@op.pl>
2023-08-05 02:40:18 +0200

    Advenced Node Course - commit 29

--- 02 Advanced Node/03_Project/server/services/cache.ts
+++ 02 Advanced Node/03_Project/server/services/cache.ts
@@ -1,12 +1,28 @@
 import mongoose from "mongoose";
+import { createClient } from "redis";
+
+//* Redis
+const client = createClient({
+  url: `redis://:${process.env.RedisSecret}@${process.env.RedisHost}:${process.env.RedisPort}`,
+});
+// console.log("client:", client);
+
+// await client.connect();
+// await client.disconnect();
+client.connect();
+
+//- Overwrite existing function - do wee need it???
+// client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 // console.log({ exec });
 
-mongoose.Query.prototype.exec = function () {
+mongoose.Query.prototype.exec = async function () {
+  client.on("error", (err: Error) => console.log("Redis Client Error:", err));
+
   // console.log("I'm about to run a query");
   // console.log(this.getQuery());
-  // @ts-ignore
+  // // @ts-ignore
   // console.log(this.mongooseCollection.name);
 
   const key = JSON.stringify(
@@ -15,7 +31,20 @@ mongoose.Query.prototype.exec = function () {
       collection: this.mongooseCollection.name,
     })
   );
-  console.log("key:", key);
+  // console.log("key:", key);
 
-  return exec.apply(this, arguments as any);
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.get(key);
+  // If we do, return that
+  if (cacheValue) {
+    console.log("cacheValue:", cacheValue);
+    // const doc = JSON.parse(cacheValue);
+    // return Array.isArray(doc) ? doc.map((d) => new this.model(d)) : new this.model(doc);
+  }
+
+  // Otherwise, issue the query and store the result in redis
+  const result = await exec.apply(this, arguments as any);
+  console.log("result:", result);
+
+  return result;
 };
