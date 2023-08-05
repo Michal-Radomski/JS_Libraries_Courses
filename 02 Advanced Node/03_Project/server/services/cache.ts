efa2b065ea95cfc8fad42b69db7bd6185722f710
Michal Radomski <michalra@op.pl>
2023-08-05 02:13:56 +0200

    Advenced Node Course - commit 27

--- 02 Advanced Node/03_Project/server/services/cache.ts
+++ 02 Advanced Node/03_Project/server/services/cache.ts
@@ -4,6 +4,10 @@ const exec = mongoose.Query.prototype.exec;
 // console.log({ exec });
 
 mongoose.Query.prototype.exec = function () {
-  console.log("I'm about to run a query");
+  // console.log("I'm about to run a query");
+  // console.log(this.getQuery());
+  // @ts-ignore
+  console.log(this.mongooseCollection.name);
+
   return exec.apply(this, arguments as any);
 };
