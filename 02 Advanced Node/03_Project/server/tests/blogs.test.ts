804347f1bc6dcfa63d498bc1ac13088e18f59d4b
Michal Radomski <michalra@op.pl>
2023-08-12 23:26:00 +0200

    Advanced Node Course - commit 70

--- 02 Advanced Node/03_Project/server/tests/blogs.test.ts
+++ 02 Advanced Node/03_Project/server/tests/blogs.test.ts
@@ -78,35 +78,59 @@ describe("When logged in, we", () => {
 });
 
 describe("user is not logged in", () => {
-  test("user can't create blog post", async () => {
-    const result = await customPage.evaluate(() => {
-      return fetch("/api/blogs", {
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          title: "My title3",
-          content: "My Content3",
-        }),
-      }).then((res: Response) => res.json());
-    });
-    // console.log("result:", result);
-    expect(result).toEqual({ error: "You must log in!" });
-  });
+  //* V1
+  // test("user can't create blog post", async () => {
+  //   const result = await customPage.evaluate(() => {
+  //     return fetch("/api/blogs", {
+  //       method: "POST",
+  //       credentials: "same-origin",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         title: "My title3",
+  //         content: "My Content3",
+  //       }),
+  //     }).then((res: Response) => res.json());
+  //   });
+  //   // console.log("result:", result);
+  //   expect(result).toEqual({ error: "You must log in!" });
+  // });
 
-  test("user can't get a list of posts", async () => {
-    const result = await customPage.evaluate(() => {
-      return fetch("/api/blogs", {
-        method: "GET",
-        credentials: "same-origin",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }).then((res: Response) => res.json());
-    });
-    // console.log("result:", result);
-    expect(result).toEqual({ error: "You must log in!" });
+  // test("user can't get a list of posts", async () => {
+  //   const result = await customPage.evaluate(() => {
+  //     return fetch("/api/blogs", {
+  //       method: "GET",
+  //       credentials: "same-origin",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     }).then((res: Response) => res.json());
+  //   });
+  //   // console.log("result:", result);
+  //   expect(result).toEqual({ error: "You must log in!" });
+  // });
+
+  //* V2 -> shorter version!
+  const actions = [
+    {
+      method: "get",
+      path: "/api/blogs",
+    },
+    {
+      method: "post",
+      path: "/api/blogs",
+      data: {
+        title: "Title 4",
+        content: "Content 4",
+      },
+    },
+  ];
+
+  test("blog related actions are prohibited", async () => {
+    const results = await customPage.execRequests(actions);
+    for (let result of results) {
+      expect(result).toEqual({ error: "You must log in!" });
+    }
   });
 });
--- 02 Advanced Node/03_Project/server/tests/helpers/pageTS.ts
+++ 02 Advanced Node/03_Project/server/tests/helpers/pageTS.ts
@@ -56,6 +56,43 @@ class CustomPage {
   async getContentsOf(selector: string) {
     return this.page.$eval(selector, (el) => el.innerHTML);
   }
+
+  get(path: string) {
+    return this.page.evaluate((_path) => {
+      return fetch(_path, {
+        method: "GET",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json());
+    }, path);
+  }
+
+  post(path: string, data: Object) {
+    return this.page.evaluate(
+      (_path, _data) => {
+        return fetch(_path, {
+          method: "POST",
+          credentials: "same-origin",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(_data),
+        }).then((res) => res.json());
+      },
+      path,
+      data
+    );
+  }
+
+  execRequests(actions: { method: string; path: string; data: Object }[]) {
+    return Promise.all(
+      actions.map(({ method, path, data }) => {
+        return (this as any)[method](path, data);
+      })
+    );
+  }
 }
 
 module.exports = CustomPage;
