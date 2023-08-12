f13dc434dcc7a9d2b44d0770411c6f10621c6221
Michal Radomski <michalra@op.pl>
2023-08-12 23:09:42 +0200

    Advanced Node Course - commit 68

--- 02 Advanced Node/03_Project/_exercises/fetchAPI.ts
+++ 02 Advanced Node/03_Project/_exercises/fetchAPI.ts
@@ -11,3 +11,13 @@
     }),
   });
 };
+
+() => {
+  fetch("/api/blogs", {
+    method: "GET",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
