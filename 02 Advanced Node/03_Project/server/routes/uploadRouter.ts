b1b6849a84fd19b57e420a454eaf23183dbba7a4
Michal Radomski <michalra@op.pl>
2023-08-14 20:11:13 +0200

    Advanced Node Course - commit 82

--- 02 Advanced Node/03_Project/server/routes/uploadRouter.ts
+++ 02 Advanced Node/03_Project/server/routes/uploadRouter.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, Router } from "express";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { v4 as uuidv4 } from "uuid";
 
+import { requireLogin } from "./../middlewares/requireLogin";
 const keys = require("../config/keys");
 
 const client = new S3Client({
@@ -12,9 +13,9 @@ const client = new S3Client({
     secretAccessKey: keys.AWS_secretKeyID,
   },
 });
-console.log("client:", client);
+// console.log("client:", client);
 
-const uploadRouter: express.Router = express.Router();
+const uploadRouter: Router = express.Router();
 
 interface CustomUser extends Express.User {
   id?: string;
@@ -24,7 +25,7 @@ export interface CustomRequest extends Request {
   user?: CustomUser;
 }
 
-uploadRouter.get("/api/upload", async (req: CustomRequest, res: Response) => {
+uploadRouter.get("/api/upload", requireLogin, async (req: CustomRequest, res: Response) => {
   console.log("req.ip:", req.ip);
 
   const key = `${req.user!.id!}/${uuidv4()}.jpeg`;
@@ -37,10 +38,10 @@ uploadRouter.get("/api/upload", async (req: CustomRequest, res: Response) => {
   });
   console.log("command:", command);
 
-  const url = await getSignedUrl(client, command, { expiresIn: 3600 });
+  const url = await getSignedUrl(client, command, { expiresIn: 180 });
   console.log({ url });
 
   await res.send(url);
-}) as express.Router;
+}) as Router;
 
 export default uploadRouter;
