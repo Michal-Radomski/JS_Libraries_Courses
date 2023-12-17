594962a2635b40e12a731f6c388300fa067d17f4
Michal Radomski <michalra@op.pl>
2023-12-17 02:15:23 +0100

    React Libraries course - commit 02

--- 04 React Libraries/src/App.tsx
+++ 04 React Libraries/src/App.tsx
@@ -3,7 +3,14 @@ import React from "react";
 import "./App.scss";
 
 const App = (): JSX.Element => {
-  return <React.Fragment>App</React.Fragment>;
+  const [counter, setCounter] = React.useState<number>(0);
+  return (
+    <React.Fragment>
+      Counter: {counter}
+      <button onClick={() => setCounter((prev) => prev + 1)}> + </button>
+      <button onClick={() => setCounter((prev) => prev - 1)}> - </button>
+    </React.Fragment>
+  );
 };
 
 export default App;
