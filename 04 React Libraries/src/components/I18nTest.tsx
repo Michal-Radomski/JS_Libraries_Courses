27f567d75065ae9b4dc85bb78653f1660b1f762d
Michal Radomski <michalra@op.pl>
2023-12-21 02:28:39 +0100

    React Libraries course - commit 27

--- 04 React Libraries/src/components/I18nTest.tsx
+++ 04 React Libraries/src/components/I18nTest.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const I18nTest = (): JSX.Element => {
-  return <React.Fragment>I18nTest</React.Fragment>;
+  const { t, i18n } = useTranslation();
+
+  return (
+    <React.Fragment>
+      <h1>{t("dogs", { count: 21, ordinal: true })}</h1>
+      <h2>{t("included", { count: 10, context: "random" })}</h2>
+      <p>{t("thereAreSomeCats", { count: 2 })}</p>
+      <button onClick={() => i18n.changeLanguage(i18n.language === "en" ? "it" : "en")}>
+        Change to {i18n.language === "en" ? "Italian" : "English"}
+      </button>
+      <p>{t("test.languageTest")}</p>
+    </React.Fragment>
+  );
 };
 
 export default I18nTest;
