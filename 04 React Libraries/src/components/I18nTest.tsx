76ae05316337c1751c292ec7b188cb729b92a2d5
Michal Radomski <michalra@op.pl>
2023-12-21 18:00:37 +0100

    React Libraries course - commit 29

--- 04 React Libraries/src/components/I18nTest.tsx
+++ 04 React Libraries/src/components/I18nTest.tsx
@@ -3,11 +3,15 @@ import { useTranslation } from "react-i18next";
 
 const I18nTest = (): JSX.Element => {
   const { t, i18n } = useTranslation();
-
   // console.log("i18n.languages:", i18n.languages);
+  const user = {
+    name: "Michal",
+    surname: "Radomski",
+  };
 
   return (
     <React.Fragment>
+      <h1>{t("helloName", { name: user.name, surname: user.surname })}</h1>
       <h1>{t("dogs", { count: 21, ordinal: true })}</h1>
       <h2>{t("included", { count: 10, context: "random" })}</h2>
       <p>{t("thereAreSomeCats", { count: 2 })}</p>
