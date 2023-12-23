3b3f5b3efbcc0a58ec1f40aa7eba0c04bbc59c53
Michal Radomski <michalra@op.pl>
2023-12-23 15:07:50 +0100

    Webpack 5 and Vite course - commit 21

--- 05 Webpack5 and Vite/04 Vite/src/main.ts
+++ 05 Webpack5 and Vite/04 Vite/src/main.ts
@@ -1,10 +1,17 @@
 import "./style.scss";
-import { setupCounter } from "./counter.ts";
+// import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-`;
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+// `;
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+
+const btn = document.getElementById("btn") as HTMLButtonElement;
+btn.onclick = () => {
+  import("./click.ts").then(({ default: click }) => {
+    click();
+  });
+};
