36ba5313a506ca5988ed72d7d0b492820d011d61
Michal Radomski <michalra@op.pl>
2023-12-23 01:54:55 +0100

    Webpack 5 and Vite course - commit 18

---
+++ 05 Webpack5 and Vite/03/webpack.dev.js
@@ -0,0 +1,39 @@
+const { merge } = require("webpack-merge");
+const path = require("path");
+const html = require("html-webpack-plugin");
+
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  context: path.resolve(__dirname, "src"),
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|png|jpeg|gif)$/i,
+        type: "asset/resource", // file-loader
+        generator: {
+          filename: "IMG/[hash].[name][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new html({
+      filename: "index.html",
+      template: "./template/tpl.html",
+      title: "Asset Modules",
+      minify: false,
+      favicon: "./assets/favicon.ico",
+    }),
+  ],
+});
