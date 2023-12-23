0aa2f2010fd13882b0d6d7886f2c176019979294
Michal Radomski <michalra@op.pl>
2023-12-23 16:56:22 +0100

    Webpack 5 and Vite course - commit 25

--- 05 Webpack5 and Vite/05/vite.config.cjs
+++ 05 Webpack5 and Vite/05/vite.config.cjs
@@ -5,19 +5,28 @@ module.exports = defineConfig({
   //mode
   //root:process.cwd(),
   root: path.resolve(__dirname, "src"), // ./ -> process.cwd()
-  //base:'/',
+  // base:'/apple/',
+  base: "/",
   //publicDir:'public', // false to disable; ./ -> vite root
   publicDir: path.resolve(__dirname, "public"),
   build: {
     target: "modules",
     minify: "esbuild",
     emptyOutDir: true,
+    cssCodeSplit: false,
+    assetsInlineLimit: 0,
     //outDir:'dist', // ./ -> vite root
     outDir: path.resolve(__dirname, "dist"),
     assetsDir: "myAssets", // relative to outDir
     rollupOptions: {
-      //input:{}, default: index: vite root / index.html
-      //output:{}
+      input: {
+        main: path.resolve(__dirname, "./src/index.html"),
+      },
+      output: {
+        entryFileNames: "ENTRY/[name].[hash].[format].ini.js",
+        chunkFileNames: "[name].[hash].[format].chunk.js",
+        assetFileNames: "[name].[hash].asset.[ext]",
+      },
     },
   },
 });
