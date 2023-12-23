a04a6c9a9afdb455209236430120b74cc171cc78
Michal Radomski <michalra@op.pl>
2023-12-24 00:53:11 +0100

    Webpack 5 and Vite course - commit 29 - course done

---
+++ 05 Webpack5 and Vite/08 Lerna/local/webpack.config.js
@@ -0,0 +1,28 @@
+//* Local
+const path = require("path");
+const { ModuleFederationPlugin } = require("webpack").container;
+
+module.exports = (_env, argv) => {
+  return {
+    context: path.resolve(__dirname, "src"),
+    entry: "./entry.js",
+    output: {
+      clean: true,
+      path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+      port: 3333,
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "home_container",
+        remotes: {
+          remote_path:
+            argv.mode === "production"
+              ? "remote_container@http://localhost:8888/remote/remoteEntry.js"
+              : "remote_container@http://localhost:4444/remoteEntry.js",
+        },
+      }),
+    ],
+  };
+};
