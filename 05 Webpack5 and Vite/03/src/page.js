613343d35bf4fcaed76f8391b91c07ef087d5330
Michal Radomski <michalra@op.pl>
2023-12-23 01:02:57 +0100

    Webpack 5 and Vite course - commit 14

--- 05 Webpack5 and Vite/03/src/page.js
+++ 05 Webpack5 and Vite/03/src/page.js
@@ -6,5 +6,7 @@ import Txt from "./assets/London.txt";
 let p = document.getElementById("p");
 p.innerText = Txt;
 
-import bg from "./assets/bg.svg";
-document.body.style.backgroundImage = `url(${bg})`;
+// import bg from "./assets/bg.svg";
+// document.body.style.backgroundImage = `url(${bg})`;
+
+import "./css/page.css";
--- 05 Webpack5 and Vite/03/webpack.config.js
+++ 05 Webpack5 and Vite/03/webpack.config.js
@@ -1,42 +1,68 @@
-const path = require('path')
-const html = require('html-webpack-plugin')
+const path = require("path");
+const html = require("html-webpack-plugin");
+const css = require("mini-css-extract-plugin");
+const cssMinimizer = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-    context:path.resolve(__dirname,'src'),
-    mode:'production',
-    entry:'./page.js',
-    optimization:{
-        chunkIds:'named',// default to 'deterministic' under prod. mode
-    },
-    output:{
-        clean:true,
-        filename:'[name].ini.js',
-        chunkFilename:'[id].chunk.js',
-        path:path.resolve(__dirname,'dist')
-    },
-    module:{
-        rules:[
-            {
-                test:/\.txt$/i,
-                type:'asset/source', // raw-loader
-            },
-            {
-                test:/\.(jpg|png|jpeg|gif)$/i,
-                type:'asset/resource', // file-loader
-            },
-            {
-                test:/\.svg$/i,
-                type:'asset/inline', // url-loader
-            }
-        ]
-    },
-    plugins:[
-        new html({
-            filename:'index.html',
-            template:'./template/tpl.html',
-            title:'Asset Modules',
-            minify:false,
-            favicon:'./assets/favicon.ico'
-        }),
-    ]
-}
+  context: path.resolve(__dirname, "src"),
+  mode: "production",
+  entry: "./page.js",
+  optimization: {
+    chunkIds: "named", // default to 'deterministic' under prod. mode
+    minimizer: ["...", new cssMinimizer()],
+  },
+  output: {
+    clean: true,
+    filename: "[name].ini.js",
+    // chunkFilename: "[id].chunk.js",
+    assetModuleFilename: "assets/[name].[hash][ext]",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"], //* This order is required!
+      },
+      {
+        test: /\.txt$/i,
+        type: "asset/source", //* raw-loader
+      },
+      {
+        test: /\.(jpg|png|jpeg|gif)$/i,
+        type: "asset/resource", //* file-loader
+        generator: {
+          filename: "IMG/[hash].[name][ext]",
+          publicPath: "/apple/",
+          outputPath: "apple/",
+        },
+      },
+      //   {
+      //     test: /\.svg$/i,
+      //     type: "asset/inline", //* url-loader -> smaller files
+      //   },
+      {
+        test: /\.svg$/i,
+        type: "asset",
+        // parser:{
+        //     dataUrlCondition:{
+        //         maxSize:10, //* bytes
+        //     }
+        // }
+      },
+    ],
+  },
+  plugins: [
+    new css({
+      filename: "[name].css",
+      // chunkFilename
+    }),
+    new html({
+      filename: "index.html",
+      template: "./template/tpl.html",
+      title: "Asset Modules",
+      minify: false,
+      favicon: "./assets/favicon.ico",
+    }),
+  ],
+};
