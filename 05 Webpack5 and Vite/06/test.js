824ec1116b14e4f94f2d1ce3b425bc8638c6a509
Michal Radomski <michalra@op.pl>
2023-12-24 00:07:29 +0100

    Webpack 5 and Vite course - commit 27

--- 05 Webpack5 and Vite/06/test.js
+++ 05 Webpack5 and Vite/06/test.js
@@ -1,4 +1,2 @@
-import * as a from "./dist/my-lib.js";
-import * as b from "./dist/my-lib.cjs";
-import * as c from "./dist/my-lib.umd.cjs";
-console.log(a, b, c);
+const m = require("./dist/main.js");
+console.log({ m });
