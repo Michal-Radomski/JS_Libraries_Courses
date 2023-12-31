3b20c1c95a1fd3f814088087d05f76a46194d118
Michal Radomski <michalra@op.pl>
2023-12-31 02:35:08 +0100

    Webpack 5 course - commit 18

--- 06 Webpack5/03/src/server.ts
+++ 06 Webpack5/03/src/server.ts
@@ -1,14 +1,31 @@
 import http from "http";
+import path from "path";
+import fs from "fs";
 
 import express, { Express, Request, Response } from "express";
 
 // The server
 const app: Express = express();
+app.use("/", express.static(path.resolve(__dirname, "../dist")));
 
 // Test route
+// app.get("/", (req: Request, res: Response) => {
+//   console.log("req.ip:", req.ip);
+//   res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+// });
+
+//* Favicon -> unnecessary - served by index.html!
+// app.get("/favicon.ico", (_req: Request, res: Response) => {
+//   // console.log("__dirname:", __dirname);
+//   res.sendFile(path.join(__dirname, "../dist/favicon.svg"));
+// });
+
+// Serving the Page
 app.get("/", (req: Request, res: Response) => {
   console.log("req.ip:", req.ip);
-  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/index.html");
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+  res.send(contentFromHtmlFile);
 });
 
 // Port
