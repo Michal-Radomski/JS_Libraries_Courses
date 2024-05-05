8be06dbfed05b9d7f8ec7a7549524b50112f0a24
Michal Radomski <michalra@op.pl>
2024-05-05 23:36:18 +0200

    Electron course - commit 03

--- 07 Electron/01_video-info/index.ts
+++ 07 Electron/01_video-info/index.ts
@@ -1,9 +1,12 @@
 import electron from "electron";
 // console.log("electron:", electron);
 
-const { app } = electron;
+const { app, BrowserWindow } = electron;
 // console.log("app:", app);
 
 app.on("ready", () => {
-  console.log("App is ready");
+  // console.log("App is ready");
+  const mainWindow = new BrowserWindow({});
+  // console.log("mainWindow:", mainWindow);
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
