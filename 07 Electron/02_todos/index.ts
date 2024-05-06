9bfc0100041bd09ccd6b8f1d134b928109a10041
Michal Radomski <michalra@op.pl>
2024-05-07 01:12:52 +0200

    Electron course - commit 10

--- 07 Electron/02_todos/index.ts
+++ 07 Electron/02_todos/index.ts
@@ -1,11 +1,16 @@
 import electron from "electron";
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({
     webPreferences: { nodeIntegration: true, contextIsolation: false },
   });
 
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+const menuTemplate = [{ label: "File" }];
