1ea5645275008bdbe50952dfdf0a58dafba03b81
Michal Radomski <michalra@op.pl>
2024-05-07 01:03:24 +0200

    Electron course - commit 09

---
+++ 07 Electron/02_todos/index.ts
@@ -0,0 +1,11 @@
+import electron from "electron";
+
+const { app, BrowserWindow } = electron;
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: { nodeIntegration: true, contextIsolation: false },
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
