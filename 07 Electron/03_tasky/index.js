2b33d78ffe49a626266d4b394b3cbaa8b095ae05
Michal Radomski <michalra@op.pl>
2024-05-09 23:16:35 +0200

    Electron course - commit 23

--- 07 Electron/03_tasky/index.js
+++ 07 Electron/03_tasky/index.js
@@ -1,15 +1,22 @@
 const electron = require("electron");
+const path = require("path");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
+let tray;
 
 app.on("ready", () => {
   mainWindow = new BrowserWindow({
     height: 500,
     width: 300,
-    frame: false,
+    frame: true,
     resizable: false,
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+  // const iconName = process.platform === "win32" ? "windows-icon.png" : "linux" ? "windows-icon.png" : "iconTemplate.png";
+  const iconName = process.platform === "win32" || "linux" ? "windows-icon.png" : "iconTemplate.png";
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+  tray = new Tray(iconPath);
 });
