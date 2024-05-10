7747f22a1497b48dbd2c056e0c0c12e10e6eb4bc
Michal Radomski <michalra@op.pl>
2024-05-11 01:48:17 +0200

    Electron course - commit 35

---
+++ 07 Electron/04_converter/src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers, legacy_createStore as createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import videosReducer from "./videos_reducer";
+
+const rootReducer = combineReducers({
+  videos: videosReducer,
+});
+
+const store = createStore(rootReducer, {}, applyMiddleware(thunk));
+
+export default store;
