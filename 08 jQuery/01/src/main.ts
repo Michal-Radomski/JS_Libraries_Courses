6c0e5af0ef974a9a49b4845c5c07aa0d6bbc4e14
Michal Radomski <michalra@op.pl>
2024-05-12 23:58:13 +0200

    jQuery course - commit 03

--- 08 jQuery/01/src/main.ts
+++ 08 jQuery/01/src/main.ts
@@ -10,6 +10,10 @@ declare global {
 //   console.log("Hello World!");
 // });
 
+// $(function () {
+//   console.log("ready!");
+// });
+
 // $(document).ready(function () {
 //   $("*").css("background-color", "#b3ffff");
 // });
@@ -22,10 +26,29 @@ declare global {
 //   $(".container-fluid").css("background-color", "#b3ffff");
 // });
 
-$(document).ready(function () {
-  $("#p-id").css("background-color", "#b3ffff");
-});
+// $(document).ready(function () {
+//   $("#p-id").css("background-color", "#b3ffff");
+// });
 
-$(document).ready(function () {
-  $("h1, p").css("background-color", "#b3ffff");
+// $(document).ready(function () {
+//   $("h1, p").css("background-color", "#b3ffff");
+// });
+
+$(document).ready(() => {
+  $("#bob").click(function () {
+    $(".bob").css("background-color", "#999966");
+    $("button").css({
+      "border-color": "#669999",
+      "border-width": "5px",
+    });
+  });
+
+  $("#carol").click(function () {
+    $(".carol").css("background-color", "#9999ff");
+  });
+
+  $("#bg").click(function () {
+    $("*").css("background-color", "#9999ff");
+    $(".bob , .carol").css("color", "white");
+  });
 });
