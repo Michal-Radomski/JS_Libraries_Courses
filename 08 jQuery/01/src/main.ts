c78d3f4e91bca824a11c42f9c33e4c7e4af49afc
Michal Radomski <michalra@op.pl>
2024-05-12 23:37:53 +0200

    jQuery course - commit 01

--- 08 jQuery/01/src/main.ts
+++ 08 jQuery/01/src/main.ts
@@ -1,7 +1,15 @@
-import $ from "jquery";
-
 import "./style.scss";
 
+declare global {
+  interface Window {
+    $: JQueryStatic;
+  }
+}
+
+// $(document).ready(function () {
+//   console.log("Hello World!");
+// });
+
 $(document).ready(function () {
-  console.log("Hello World!");
+  $("*").css("background-color", "#b3ffff");
 });
