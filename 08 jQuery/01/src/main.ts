51ad4370a953f7bf975853af1e28763edd95b8e7
Michal Radomski <michalra@op.pl>
2024-05-13 00:13:52 +0200

    jQuery course - commit 04

--- 08 jQuery/01/src/main.ts
+++ 08 jQuery/01/src/main.ts
@@ -14,6 +14,7 @@ declare global {
 //   console.log("ready!");
 // });
 
+//* Selectors
 // $(document).ready(function () {
 //   $("*").css("background-color", "#b3ffff");
 // });
@@ -34,21 +35,40 @@ declare global {
 //   $("h1, p").css("background-color", "#b3ffff");
 // });
 
-$(document).ready(() => {
-  $("#bob").click(function () {
-    $(".bob").css("background-color", "#999966");
-    $("button").css({
-      "border-color": "#669999",
-      "border-width": "5px",
-    });
-  });
+// $(document).ready(() => {
+//   $("#bob").click(function () {
+//     $(".bob").css("background-color", "#999966");
+//     $("button").css({
+//       "border-color": "#669999",
+//       "border-width": "5px",
+//     });
+//   });
 
-  $("#carol").click(function () {
-    $(".carol").css("background-color", "#9999ff");
-  });
+//   $("#carol").click(function () {
+//     $(".carol").css({ "background-color": "#9999ff" });
+//   });
 
-  $("#bg").click(function () {
-    $("*").css("background-color", "#9999ff");
-    $(".bob , .carol").css("color", "white");
+//   $("#bg").click(function () {
+//     $("*").css("background-color", "#9999ff");
+//     $(".bob , .carol").css("color", "white");
+//   });
+// });
+
+//* Attribute
+// $(document).ready(function () {
+//   $("[id]").each(function () {
+//     console.log("this:", this);
+//     $(this).css("background-color", "#b3ffff");
+//   });
+// });
+
+$(document).ready(function () {
+  const idName = $("[id=nameOne]");
+  const className = $("[class=nameTwo]");
+
+  idName.css("background-color", "CadetBlue");
+
+  className.each(function () {
+    $(this).css("background-color", "#b3ffff");
   });
 });
