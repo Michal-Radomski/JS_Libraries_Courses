1fa3f73170c76a2583706c7f6354d604a3cea484
Michal Radomski <michalra@op.pl>
2024-05-24 22:48:15 +0200

    Deno course - commit 01

--- 09 Deno/main.ts
+++ 09 Deno/main.ts
@@ -1,1 +1,18 @@
-console.log("test");
+//* deno run --allow-net main.ts
+
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const port = 8000;
+const app = new Application();
+// console.log("app:", app);
+
+app.use((ctx, next) => {
+  ctx.response.body = "Hello Deno";
+  next();
+});
+
+app.addEventListener("listen", () => {
+  console.log(`Listening on localhost:${port}`);
+});
+
+await app.listen({ port });
