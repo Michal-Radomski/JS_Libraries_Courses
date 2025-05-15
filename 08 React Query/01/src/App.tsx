0b1f8f71c360536943a671a327422065c10b9ebd
Michal Radomski <michalra@op.pl>
2025-05-15 22:44:18 +0200

    React Query course - commit 04

--- 08 React Query/01/src/App.tsx
+++ 08 React Query/01/src/App.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "./App.scss";
 import Posts from "./Posts";
 
+const queryClient: QueryClient = new QueryClient();
+
 const App = (): React.JSX.Element => {
   return (
     <React.Fragment>
-      <div className="App">
-        <h1>Blog &apos;em Ipsum</h1>
-        <Posts />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <h1>Blog Posts</h1>
+          <Posts />
+        </div>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </React.Fragment>
   );
 };
--- 08 React Query/01/src/Posts.tsx
+++ 08 React Query/01/src/Posts.tsx
@@ -1,20 +1,55 @@
 import React from "react";
+import { useQuery, useQueryClient, useMutation, QueryClient } from "@tanstack/react-query";
 
-// import { fetchPosts, deletePost, updatePost } from "./api";
+import { fetchPosts, deletePost, updatePost } from "./api";
 import PostDetail from "./PostDetail";
-// const maxPostPage = 10;
+const maxPostPage = 10;
 
 const Posts = (): React.JSX.Element => {
   const [currentPage, setCurrentPage] = React.useState<number>(0);
   const [selectedPost, setSelectedPost] = React.useState<Post | null>(null);
 
-  // replace with useQuery
-  const data = [] as Post[];
+  const queryClient: QueryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: (postId: string) => deletePost(postId),
+  });
+
+  const updateMutation = useMutation({
+    mutationFn: (postId: string) => updatePost(postId),
+  });
+
+  React.useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery({
+        queryKey: ["posts", nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      });
+    }
+  }, [currentPage, queryClient]);
+
+  const { data, isError, error, isLoading } = useQuery({
+    queryKey: ["posts", currentPage],
+    queryFn: () => fetchPosts(currentPage),
+    staleTime: 2000, // 2 seconds
+  });
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  if (isError) {
+    return (
+      <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <React.Fragment>
       <ul>
-        {data.map((post: Post) => (
+        {data?.map((post: Post) => (
           <li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
             {post.title}
           </li>
