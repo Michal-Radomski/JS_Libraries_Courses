b17b8d6f2277be83d7a176d24d547796002e703e
Michal Radomski <michalra@op.pl>
2025-05-16 22:45:07 +0200

    React Query course - commit 10

---
+++ 08 React Query/02/src/Interfaces.d.ts
@@ -0,0 +1,11 @@
+interface Person {
+  name: string;
+  hair_color: string;
+  eye_color: string;
+}
+
+interface Spece {
+  name: string;
+  language: string;
+  average_lifespan: number;
+}
--- 08 React Query/02/src/species/InfiniteSpecies.tsx
+++ 08 React Query/02/src/species/InfiniteSpecies.tsx
@@ -1,15 +1,55 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
-// import InfiniteScroll from "react-infinite-scroller";
+import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-// import { Species } from "./Species";
+import { Species } from "./Species";
 
-// const initialUrl = "https://swapi.dev/api/species/";
-// const fetchUrl = async (url:string) => {
-//   const response = await fetch(url);
-//   return response.json();
-// };
+const initialUrl: string = "https://swapi.py4e.com/api/species/";
+const fetchUrl = async (url: string) => {
+  const response = await fetch(url);
+  return response.json();
+};
 
 export function InfiniteSpecies(): React.JSX.Element {
-  // TODO: get data for InfiniteScroll via React Query
-  return <React.Fragment>{/* <InfiniteScroll /> */}</React.Fragment>;
+  const { data, fetchNextPage, hasNextPage, isLoading, isFetching, isError, error } = useInfiniteQuery({
+    queryKey: ["sw-species"],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage: { next: boolean }) => lastPage.next || undefined,
+  } as any);
+
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error! {error.toString()}</div>;
+  }
+
+  return (
+    <React.Fragment>
+      {isFetching ? <div className="loading">Loading...</div> : null}
+
+      <InfiniteScroll
+        loadMore={() => {
+          if (!isFetching) fetchNextPage();
+        }}
+        hasMore={hasNextPage}
+        initialLoad={false} // Note: add `initialLoad={false}` to avoid double-load of page 2.
+      >
+        {data.pages.map((pageData: any) => {
+          return (pageData?.results as Spece[]).map((species: Spece) => {
+            return (
+              <Species
+                key={species.name}
+                name={species.name}
+                language={species.language}
+                averageLifespan={species.average_lifespan}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </React.Fragment>
+  );
 }
