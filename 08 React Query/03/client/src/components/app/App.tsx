2abc5771c980064b33ff3140ceacc4b6ac3127cf
Michal Radomski <michalra@op.pl>
2025-05-21 23:48:31 +0200

    React Query course - commit 14

--- 08 React Query/03/client/src/components/app/App.tsx
+++ 08 React Query/03/client/src/components/app/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
@@ -15,28 +14,25 @@ import { AllStaff } from "@/components/staff/AllStaff";
 import { Treatments } from "@/components/treatments/Treatments";
 import { Signin } from "@/components/user/Signin";
 import { UserProfile } from "@/components/user/UserProfile";
-import { theme } from "@/theme";
 
 export function App() {
   return (
     <React.Fragment>
-      <ChakraProvider theme={theme}>
-        <AuthContextProvider>
-          <Loading />
-          <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/Staff" element={<AllStaff />} />
-              <Route path="/Calendar" element={<Calendar />} />
-              <Route path="/Treatments" element={<Treatments />} />
-              <Route path="/signin" element={<Signin />} />
-              <Route path="/user/:id" element={<UserProfile />} />
-            </Routes>
-          </BrowserRouter>
-          <ToastContainer />
-        </AuthContextProvider>
-      </ChakraProvider>
+      <AuthContextProvider>
+        <Loading />
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Staff" element={<AllStaff />} />
+            <Route path="/Calendar" element={<Calendar />} />
+            <Route path="/Treatments" element={<Treatments />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/user/:id" element={<UserProfile />} />
+          </Routes>
+        </BrowserRouter>
+        <ToastContainer />
+      </AuthContextProvider>
 
       <ReactQueryDevtools />
     </React.Fragment>
--- 08 React Query/03/client/src/main.tsx
+++ 08 React Query/03/client/src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import { theme } from "./theme";
 import { App } from "@/components/app/App";
+import { queryClient } from "./react-query/queryClient";
 
-const queryClient = new QueryClient();
 const root = document.getElementById("root") as HTMLDivElement;
 
 ReactDOM.createRoot(root).render(
