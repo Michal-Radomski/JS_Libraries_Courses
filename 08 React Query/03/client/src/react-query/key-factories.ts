82afe809227979aced3723ace83405413948da78
Michal Radomski <michalra@op.pl>
2025-05-23 23:55:00 +0200

    React Query course - commit 20

---
+++ 08 React Query/03/client/src/react-query/key-factories.ts
@@ -0,0 +1,11 @@
+import { queryKeys } from "./constants";
+
+export const generateUserKey = (userId: number, _userToken: string) => {
+  // deliberately exclude the userToken from the dependency array
+  //   to keep key consistent for userId regardless of token changes
+  return [queryKeys.user, userId];
+};
+
+export const generateUserAppointmentsKey = (userId: number, userToken: string) => {
+  return [queryKeys.appointments, queryKeys.user, userId, userToken];
+};
