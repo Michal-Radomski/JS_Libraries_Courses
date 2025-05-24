3625c2846063a79d01f2d8e05a6508b52964de8d
Michal Radomski <michalra@op.pl>
2025-05-24 23:25:47 +0200

    React Query course - commit 28

--- 08 React Query/03/client/src/components/staff/tests/AllStaff.test.tsx
+++ 08 React Query/03/client/src/components/staff/tests/AllStaff.test.tsx
@@ -2,9 +2,10 @@ import { AllStaff } from "../AllStaff";
 
 import { render, screen } from "@/test-utils";
 
-test("renders response from query", async () => {
+test("renders response from query", async (): Promise<void> => {
   render(<AllStaff />);
-  const staffNames = await screen.findAllByRole("heading", {
+
+  const staffNames: HTMLElement[] = await screen.findAllByRole("heading", {
     name: /divya|sandra|michael|mateo/i,
   });
 
