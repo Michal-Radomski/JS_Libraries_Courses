cd25b078011f1dc9fa016395af83ae2940b21289
Michal Radomski <michalra@op.pl>
2025-05-25 17:49:11 +0200

    RxJS & Observables course - commit 0

--- 09 RxJS & Observables/01/src/main.ts
+++ 09 RxJS & Observables/01/src/main.ts
@@ -2,12 +2,9 @@ import { name$, storeDataOnServer, storeDataOnServerError } from "./external";
 
 name$.subscribe((value: string) => console.log(1, { value }));
 
-storeDataOnServer("Some value").subscribe({
-  next: (value) => console.log(2, { value }),
-  error: (err) => console.log("Error when saving:", err.message),
-});
+storeDataOnServer("Some value").subscribe((value: string) => console.log({ value }));
 
 storeDataOnServerError("Some value").subscribe({
-  next: (value) => console.log(3, { value }),
-  error: (err) => console.log("Error when saving:", err.message),
+  next: (value: string) => console.log(3, { value }),
+  error: (err: Error) => console.log("Error when saving:", err.message),
 });
