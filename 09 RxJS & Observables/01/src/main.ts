f2480c15578680c324305ec10cfcedb70c194c3d
Michal Radomski <michalra@op.pl>
2025-05-25 17:35:09 +0200

    RxJS & Observables course - commit 03

--- 09 RxJS & Observables/01/src/main.ts
+++ 09 RxJS & Observables/01/src/main.ts
@@ -1,3 +1,23 @@
+import { Observable, of } from "rxjs";
+
 import "./style.scss";
 
-console.log("test");
+export const name$: Observable<string> = of("Alice", "Ben", "Charlie");
+
+export function storeDataOnServer(data: string): Observable<string> {
+  return new Observable((subscriber) => {
+    setTimeout(() => {
+      subscriber.next(`Saved successfully: ${data}`);
+      subscriber.complete();
+    }, 5000);
+  });
+}
+
+export function storeDataOnServerError(data: string): Observable<string> {
+  console.log("data:", data);
+  return new Observable((subscriber) => {
+    setTimeout(() => {
+      subscriber.error(new Error("Failure!"));
+    }, 5000);
+  });
+}
