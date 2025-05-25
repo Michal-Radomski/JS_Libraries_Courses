8f366d93219b69531102a234c693e66bc24833b1
Michal Radomski <michalra@op.pl>
2025-05-25 22:09:54 +0200

    RxJS & Observables course - commit 11

--- 09 RxJS & Observables/02/src/main.ts
+++ 09 RxJS & Observables/02/src/main.ts
@@ -11,11 +11,45 @@ import { Observable, Subscriber } from "rxjs";
 // console.log("After subscribe");
 
 //* Next Notification
+// const observable$ = new Observable<string>((subscriber: Subscriber<string>) => {
+//   console.log("Observable executed");
+//   subscriber.next("Alice");
+// });
+
+// console.log("Before subscribe");
+// observable$.subscribe((value: string) => console.log({ value }));
+// console.log("After subscribe");
+
+//* More Next Notifications
+// const observable$ = new Observable<string>((subscriber: Subscriber<string>) => {
+//   console.log("Observable executed");
+//   subscriber.next("Alice");
+//   subscriber.next("Ben");
+//   setTimeout(() => subscriber.complete(), 2000);
+// });
+
+// console.log("Before subscribe");
+// observable$.subscribe((value: string) => console.log({ value }));
+// console.log("After subscribe");
+
+//* Teardown - Complete Notification
 const observable$ = new Observable<string>((subscriber: Subscriber<string>) => {
   console.log("Observable executed");
   subscriber.next("Alice");
+  subscriber.next("Ben");
+  setTimeout(() => {
+    subscriber.next("Charlie");
+    subscriber.complete();
+  }, 2000);
+
+  return () => {
+    console.log("Teardown");
+  };
 });
 
 console.log("Before subscribe");
-observable$.subscribe((value: string) => console.log({ value }));
+observable$.subscribe({
+  next: (value) => console.log({ value }),
+  complete: () => console.log("Completed"),
+});
 console.log("After subscribe");
