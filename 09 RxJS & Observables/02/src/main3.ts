963e421a05575059dc2ddaff6ca4ddbc0a80985d
Michal Radomski <michalra@op.pl>
2025-05-26 22:27:44 +0200

    RxJS & Observables course - commit 19

--- 09 RxJS & Observables/02/src/main3.ts
+++ 09 RxJS & Observables/02/src/main3.ts
@@ -1,4 +1,5 @@
-import { from, fromEvent, interval, Observable, of, Subscriber, Subscription, timer } from "rxjs";
+import { forkJoin, from, fromEvent, interval, Observable, of, Subscriber, Subscription, timer } from "rxjs";
+import { ajax } from "rxjs/ajax";
 
 //* Off
 of("Alice", "Ben", "Charlie").subscribe((value: string) => console.log(1, { value }));
@@ -115,7 +116,7 @@ setTimeout(() => {
   const interval$ = new Observable<number>((subscriber) => {
     let counter = 0;
 
-    const intervalId = setInterval(() => {
+    const intervalId: number = setInterval(() => {
       console.log("Timeout!");
       subscriber.next(counter++);
     }, 1000);
@@ -128,7 +129,7 @@ setTimeout(() => {
     complete: () => console.log("Completed"),
   });
 
-  const subscription = interval$.subscribe({
+  const subscription: Subscription = interval$.subscribe({
     next: (value) => console.log(value),
     complete: () => console.log("Completed"),
   });
@@ -137,4 +138,47 @@ setTimeout(() => {
     subscription.unsubscribe();
     console.log("Unsubscribe");
   }, 5000);
+}
+
+{
+  //* ForkJoin
+  const randomName$ = ajax<{ [key: string]: string }>("https://jsonplaceholder.typicode.com/posts/1"); //* Changed api
+  const randomNation$ = ajax<{ [key: string]: string }>("https://jsonplaceholder.typicode.com/posts/2"); //* Changed api
+  const randomFood$ = ajax<{ [key: string]: string }>("https://jsonplaceholder.typicode.com/posts/3"); //* Changed api
+
+  // randomName$.subscribe((ajaxResponse) => console.log(ajaxResponse.response.first_name));
+  // randomNation$.subscribe((ajaxResponse) => console.log(ajaxResponse.response.capital));
+  // randomFood$.subscribe((ajaxResponse) => console.log(ajaxResponse.response.dish));
+
+  forkJoin([randomName$, randomNation$, randomFood$]).subscribe(([nameAjax, nationAjax, foodAjax]) =>
+    console.log(
+      `${nameAjax.response.title} is from ${nationAjax.response.title} and likes to eat ${foodAjax.response.title}.`
+    )
+  );
+
+  const a$ = new Observable((subscriber) => {
+    setTimeout(() => {
+      subscriber.next("A");
+      subscriber.complete();
+    }, 5000);
+
+    return () => {
+      console.log("A teardown");
+    };
+  });
+
+  const b$ = new Observable((subscriber) => {
+    setTimeout(() => {
+      subscriber.error("Failure!");
+    }, 3000);
+
+    return () => {
+      console.log("B teardown");
+    };
+  });
+
+  forkJoin([a$, b$]).subscribe({
+    next: (value) => console.log(value),
+    error: (err) => console.log("Error:", err),
+  });
 }
