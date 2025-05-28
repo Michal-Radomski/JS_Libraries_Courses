123ab177654fea97bb61250abdc6939590a8a33a
Michal Radomski <michalra@op.pl>
2025-05-28 23:23:14 +0200

    React Hook Form course - commit 03

--- 10 React-Hook-Form/01/src/App.tsx
+++ 10 React-Hook-Form/01/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import "./App.scss";
 import { FoodDeliveryForm } from "./FoodDeliveryForm";
+import { TypicalForm } from "./TypicalForm";
 
 const App = (): React.JSX.Element => {
   return (
     <React.Fragment>
       <div className="container">
         <div className="mx-5">
+          <TypicalForm />
           <FoodDeliveryForm />
         </div>
       </div>
---
+++ 10 React-Hook-Form/01/src/TypicalForm.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+type FoodDeliveryFormType = {
+  customerName: string;
+  mobile: string;
+};
+
+type FoodDeliveryFormErrorType = {
+  customerName: string;
+  mobile: string;
+};
+
+export const TypicalForm = (): React.JSX.Element => {
+  const [values, setValues] = React.useState<FoodDeliveryFormType>({
+    customerName: "",
+    mobile: "",
+  });
+  const [errors, setErrors] = React.useState<FoodDeliveryFormErrorType>({
+    customerName: "",
+    mobile: "",
+  });
+  console.log("errors:", errors);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const validateFormData = (): boolean => {
+    const tempErrors: FoodDeliveryFormErrorType = {
+      customerName: "",
+      mobile: "",
+    };
+
+    if (values.customerName == "") {
+      tempErrors.customerName = "Customer name is required.";
+    }
+    if (values.mobile == "") {
+      tempErrors.mobile = "Mobile number is required.";
+    }
+    setErrors(tempErrors);
+
+    return Object.values(tempErrors).every((x: string) => x == "");
+  };
+
+  const onSubmit = (e: React.SyntheticEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (validateFormData()) console.log("form data", values);
+    else console.log("form is invalid");
+  };
+
+  return (
+    <React.Fragment>
+      <form autoComplete="off" onSubmit={onSubmit}>
+        <div className="form-floating mb-3">
+          <input
+            type="text"
+            name="customerName"
+            className="form-control"
+            placeholder="Customer Name"
+            value={values.customerName}
+            onChange={handleInputChange}
+          />
+          <label>Customer Name</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="text"
+            name="mobile"
+            className="form-control"
+            placeholder="Mobile"
+            value={values.mobile}
+            onChange={handleInputChange}
+          />
+          <label>Mobile</label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </React.Fragment>
+  );
+};
