63a5a1d280c5feee5f4bc3c049ef51ab0869beb6
Michal Radomski <michalra@op.pl>
2025-06-01 23:14:33 +0200

    React Hook Form course - commit 32

--- 10 React-Hook-Form/01/src/components/OrderedFoodItems.tsx
+++ 10 React-Hook-Form/01/src/components/OrderedFoodItems.tsx
@@ -7,11 +7,9 @@ import type { OrderedFoodItemType } from "../types";
 
 const RenderCount: () => React.JSX.Element = getRenderCount();
 
+//* V2
 export default function OrderedFoodItems(): React.JSX.Element {
-  const {
-    register,
-    // setValue
-  } = useFormContext<{
+  const { register } = useFormContext<{
     foodItems: OrderedFoodItemType[];
   }>();
 
@@ -19,38 +17,20 @@ export default function OrderedFoodItems(): React.JSX.Element {
     name: "foodItems",
   });
 
-  const {
-    fields,
-    append,
-    // prepend, insert, swap,
-    move,
-    //  update,
-    replace,
-    remove,
-  } = useFieldArray<{
+  const { fields, append, remove } = useFieldArray<{
     foodItems: OrderedFoodItemType[];
   }>({
     name: "foodItems",
+    rules: {
+      required: {
+        value: true,
+        message: "No food in the order.",
+      },
+    },
   });
 
   const onRowAdd = (): void => {
     append({ name: "Food", quantity: 1 });
-    // prepend({ name: "Food", quantity: 1 })
-    // insert(2,{ name: "Food", quantity: 1 })
-  };
-
-  const onSwapAndMove = (): void => {
-    // swap(0, 2)
-    move(0, 2);
-  };
-
-  const onUpdateAndReplace = (): void => {
-    // update(0,{ name: "Food", quantity: 10 })
-    // setValue("foodItems.0.quantity",10)
-    replace([
-      { name: "Food 1", quantity: 10 },
-      { name: "Food 2", quantity: 11 },
-    ]);
   };
 
   const onRowDelete = (index: number): void => {
@@ -81,7 +61,7 @@ export default function OrderedFoodItems(): React.JSX.Element {
                   {...register(`foodItems.${index}.name` as const, {
                     required: "This field is required.",
                   })}
-                  error={errors.foodItems ? errors.foodItems[index]?.name : undefined}
+                  error={errors.foodItems && errors.foodItems[index]?.name}
                 />
               </td>
               <td>
@@ -95,15 +75,118 @@ export default function OrderedFoodItems(): React.JSX.Element {
             </tr>
           ))}
         </tbody>
+        {errors.foodItems?.root && (
+          <tfoot>
+            <tr>
+              <td colSpan={3}>
+                <span className="error-feedback">{errors.foodItems?.root?.message}</span>
+              </td>
+            </tr>
+          </tfoot>
+        )}
       </table>
-      {fields.length >= 4 && (
-        <button type="button" className="btn btn-sm btn-secondary" onClick={onSwapAndMove}>
-          Swap and Move
-        </button>
-      )}
-      <button type="button" className="btn btn-sm btn-secondary" onClick={onUpdateAndReplace}>
-        Update and Replace
-      </button>
     </React.Fragment>
   );
 }
+
+//* V1
+// export default function OrderedFoodItems(): React.JSX.Element {
+//   const {
+//     register,
+//     // setValue
+//   } = useFormContext<{
+//     foodItems: OrderedFoodItemType[];
+//   }>();
+
+//   const { errors } = useFormState<{ foodItems: OrderedFoodItemType[] }>({
+//     name: "foodItems",
+//   });
+
+//   const {
+//     fields,
+//     append,
+//     // prepend, insert, swap,
+//     move,
+//     //  update,
+//     replace,
+//     remove,
+//   } = useFieldArray<{
+//     foodItems: OrderedFoodItemType[];
+//   }>({
+//     name: "foodItems",
+//   });
+
+//   const onRowAdd = (): void => {
+//     append({ name: "Food", quantity: 1 });
+//     // prepend({ name: "Food", quantity: 1 })
+//     // insert(2,{ name: "Food", quantity: 1 })
+//   };
+
+//   const onSwapAndMove = (): void => {
+//     // swap(0, 2)
+//     move(0, 2);
+//   };
+
+//   const onUpdateAndReplace = (): void => {
+//     // update(0,{ name: "Food", quantity: 10 })
+//     // setValue("foodItems.0.quantity",10)
+//     replace([
+//       { name: "Food 1", quantity: 10 },
+//       { name: "Food 2", quantity: 11 },
+//     ]);
+//   };
+
+//   const onRowDelete = (index: number): void => {
+//     remove(index);
+//   };
+
+//   return (
+//     <React.Fragment>
+//       <RenderCount />
+
+//       <table className="table table-borderless table-hover">
+//         <thead>
+//           <tr>
+//             <th>Food</th>
+//             <th>Quantity</th>
+//             <th>
+//               <button type="button" className="btn btn-sm btn-secondary" onClick={onRowAdd}>
+//                 + Add
+//               </button>
+//             </th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {fields.map((field, index) => (
+//             <tr key={field.id}>
+//               <td>
+//                 <TextField
+//                   {...register(`foodItems.${index}.name` as const, {
+//                     required: "This field is required.",
+//                   })}
+//                   error={errors.foodItems ? errors.foodItems[index]?.name : undefined}
+//                 />
+//               </td>
+//               <td>
+//                 <TextField type="number" min={0} {...register(`foodItems.${index}.quantity` as const)} />
+//               </td>
+//               <td>
+//                 <button type="button" className="btn btn-sm btn-outline-danger" onClick={() => onRowDelete(index)}>
+//                   DEL
+//                 </button>
+//               </td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//       {fields.length >= 4 && (
+//         <button type="button" className="btn btn-sm btn-secondary" onClick={onSwapAndMove}>
+//           Swap and Move
+//         </button>
+//       )}
+//       <button type="button" className="btn btn-sm btn-secondary" onClick={onUpdateAndReplace}>
+//         Update and Replace
+//       </button>
+//     </React.Fragment>
+//   );
+// }
