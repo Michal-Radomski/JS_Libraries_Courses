bdefc7ff813728bcc2c9492ac2336ba222fb8943
Michal Radomski <michalra@op.pl>
2025-06-04 22:17:01 +0200

    React Hook Form course - commit 44

--- 10 React-Hook-Form/02/src/App.tsx
+++ 10 React-Hook-Form/02/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useForm, type FieldErrors } from "react-hook-form";
+import { Controller, useForm, type FieldErrors } from "react-hook-form";
+import { Button, TextField } from "@mui/material";
 
 import "./App.scss";
 
@@ -10,7 +11,12 @@ type FormData = {
 };
 
 const App = (): React.JSX.Element => {
-  const { register, handleSubmit } = useForm<FormData>();
+  const { register, handleSubmit, control } = useForm<FormData>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
   const onSubmit = (data: FormData): void => {
     console.log("form data", data);
@@ -20,16 +26,27 @@ const App = (): React.JSX.Element => {
     console.log("error", err);
   };
 
+  // const { ref, ...fullNameRR } = register("fullName");
+
   return (
     <React.Fragment>
       <form autoComplete="off" noValidate onSubmit={handleSubmit(onSubmit, onError)}>
-        <input type="text" placeholder="Full Name" {...register("fullName")} />
+        {/* <input type="text" placeholder="Full Name" {...register("fullName")} /> */}
+        {/* <TextField variant="outlined" label="Full Name" {...fullNameRR} inputRef={ref} defaultValue="abc" /> */}
+        <Controller
+          name="fullName"
+          control={control}
+          rules={{ required: "This field is required." }}
+          render={({ field }) => <TextField variant="outlined" label="Full Name" {...field} inputRef={field.ref} />}
+        />
         <br />
         <input type="text" placeholder="Email" {...register("email")} />
         <br />
         <input type="password" placeholder="Password" {...register("password")} />
         <br />
-        <button type="submit">Submit</button>
+        <Button type="submit" variant="contained" color="warning">
+          Submit
+        </Button>
       </form>
     </React.Fragment>
   );
--- 10 React-Hook-Form/02/src/main.tsx
+++ 10 React-Hook-Form/02/src/main.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 import App from "./App.tsx";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 createRoot(document.getElementById("root") as HTMLDivElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
