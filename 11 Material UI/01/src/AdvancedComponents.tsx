3450f9f6a01435e7ab3576bd85cd13a695f4bba3
Michal Radomski <michalra@op.pl>
2025-06-06 23:32:06 +0200

    Material UI course - commit 12

--- 11 Material UI/01/src/AdvancedComponents.tsx
+++ 11 Material UI/01/src/AdvancedComponents.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Avatar,
   Box,
   Button,
   Card,
@@ -15,9 +16,16 @@ import {
   DialogContentText,
   DialogTitle,
   Link,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
   Modal,
   Typography,
 } from "@mui/material";
+import ImageIcon from "@mui/icons-material/Image";
+import WorkIcon from "@mui/icons-material/Work";
+import BeachAccessIcon from "@mui/icons-material/BeachAccess";
 
 const AdvancedComponents = (): JSX.Element => {
   const [dialogOpen, setDialogOpen] = React.useState<boolean>(false);
@@ -121,8 +129,58 @@ const AdvancedComponents = (): JSX.Element => {
       <div>
         <CssBaseline />
         <Container maxWidth="sm">
-          <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
+          <Box sx={{ bgcolor: "#cfe8fc", height: "10vh" }} />
         </Container>
+      </div>
+      <div>
+        <Container sx={{ background: "green" }} maxWidth="xs">
+          This is XS
+        </Container>
+        <br />
+        <Container sx={{ background: "green" }} maxWidth="sm">
+          This is SM
+        </Container>
+        <br />
+        <Container sx={{ background: "green" }} maxWidth="md">
+          This is MD
+        </Container>
+        <br />
+        <Container sx={{ background: "green" }} maxWidth="lg">
+          This is LG
+        </Container>
+        <br />
+        <Container sx={{ background: "green" }} maxWidth="xl">
+          This is XL
+        </Container>
+      </div>
+
+      <div>
+        <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <ImageIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <WorkIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Work" secondary="Jan 7, 2014" />
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <BeachAccessIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Vacation" secondary="July 20, 2014" />
+          </ListItem>
+        </List>
       </div>
     </React.Fragment>
   );
