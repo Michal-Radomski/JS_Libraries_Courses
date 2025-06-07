6de215392c1cc0a83a3e27daceba13e98e9b4fa4
Michal Radomski <michalra@op.pl>
2025-06-07 23:52:14 +0200

    Material UI course - commit 25

--- 11 Material UI/01/src/Styled.tsx
+++ 11 Material UI/01/src/Styled.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, styled, Typography } from "@mui/material";
+import { AppBar, Box, styled, Typography } from "@mui/material";
+import styledEmotion from "@emotion/styled";
 
 const CustomizedTypography = styled(Typography)`
   color: #000055;
@@ -9,12 +10,43 @@ const CustomizedTypography = styled(Typography)`
   }
 `;
 
+const SomeComp = styledEmotion.div({
+  color: "hotpink",
+});
+
+const AnotherComp = styledEmotion.div`
+  color: ${(props) => props.color};
+`;
+
+const NavBar = styled(AppBar)(({ theme }) => ({
+  marginTop: "50px",
+  background: "green",
+  padding: 1, // 1px
+  display: "flex",
+  flexDirection: "row",
+  [theme.breakpoints.down("md")]: {
+    background: "red", //* Doesn't work?
+    flexDirection: "column",
+  },
+}));
+
 const Styled = (): JSX.Element => {
   return (
     <React.Fragment>
+      <NavBar sx={{ padding: 1 }}>
+        {[1, 2, 3, 4, 5].map((item) => (
+          <div key={item}>{item}</div>
+        ))}
+      </NavBar>
+
       <Box>
         <CustomizedTypography variant="h5">Hello World</CustomizedTypography>
       </Box>
+
+      <SomeComp>
+        Some Component
+        <AnotherComp color="orangered">Another Component</AnotherComp>
+      </SomeComp>
     </React.Fragment>
   );
 };
