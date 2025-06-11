5209842dc3c1d04adddb0aa98442301781c70735
Michal Radomski <michalra@op.pl>
2025-06-11 18:03:18 +0200

    Material UI course - commit 45

---
+++ 11 Material UI/07/api/api.ts
@@ -0,0 +1,76 @@
+import path from "path";
+import http from "http";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+import compression from "compression";
+
+//* Import routes
+// import indexRouter from "./indexRouter"; // Temp
+
+//* The server
+const app: Express = express();
+
+const corsOptions = {
+  origin: true,
+  methods: ["POST", "GET", "OPTIONS"],
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization", "Accept"], // Specify
+};
+
+//* Middlewares
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("combined"));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy: false,
+  })
+);
+// Compress all responses
+app.use(compression({ level: 6 }));
+
+//* Route middleware
+// app.use("/api", indexRouter); // Temp
+
+//* Mongo DB
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((con: { connection: { host: string } }) => {
+    console.log(`MongoDB Database connected with HOST: ${con.connection.host}`);
+  })
+  .catch((error: string) => console.log("Mongo DB Error => ", error));
+
+//* Favicon
+app.get("/favicon.ico", (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname + "/favicon.svg"));
+});
+//* Test route
+app.get("/", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+});
+
+//* Port
+const portHTTP = (process.env.PORT || 5000) as number;
+
+const httpServer = http.createServer(app);
+//* IPv4
+httpServer.listen({ port: portHTTP, host: "127.0.0.1" }, () => {
+  console.log(`🚀 Server is listening at http://localhost:${portHTTP}`);
+  // For testing only
+  console.log("Current Time:", new Date().toLocaleTimeString());
+});
