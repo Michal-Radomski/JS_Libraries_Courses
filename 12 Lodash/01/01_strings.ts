c813938869bb484cea8e397c28d2936366954644
Michal Radomski <michalra@op.pl>
2025-06-12 22:50:38 +0200

    Lodash course - commit 01

---
+++ 12 Lodash/01/01_strings.ts
@@ -0,0 +1,19 @@
+import _ from "lodash";
+
+//* ToUpper ToLower
+console.log(_.toUpper("foobar"));
+console.log(_.toLower("FOOBAR"));
+
+//* UpperCase and LowerCase
+console.log(_.upperCase("iLikeProgramming")); // I LIKE PROGRAMMING
+console.log(_.lowerCase("FOOBAR"));
+
+//* UpperFirst and LowerFirst
+console.log(_.upperFirst("fooBar"));
+console.log(_.lowerFirst("Foobar"));
+
+//* Capitalize
+console.log(_.capitalize("fooBar"));
+
+//*  StartCase
+console.log(_.startCase("foo bar"));
