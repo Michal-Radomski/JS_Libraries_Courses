a20736cbf9664bd4d0f6a6f0d09d26b9e271090d
Michal Radomski <michalra@op.pl>
2025-06-13 21:37:01 +0200

    Lodash course - commit 05

--- 12 Lodash/01/02_arrays.ts
+++ 12 Lodash/01/02_arrays.ts
@@ -43,3 +43,68 @@ const objects = [{ a: 10 }, { a: 20 }, { a: 30 }];
 const objects2 = [{ a: 10 }, { a: 20 }, { a: 20 }, { a: 30 }];
 console.log(_.sortedIndexBy(objects, { a: 25 }, (i) => i.a)); // 2
 console.log(_.sortedLastIndexBy(objects2, { a: 20 }, (i) => i.a)); // 3
+
+//* SortedIndexOf and SortedLastIndexOf
+console.log(_.sortedIndexOf([1, 2, 2, 2, 3], 2));
+console.log(_.sortedIndexOf([1, 2, 2, 2, 3], 3));
+console.log(_.sortedLastIndexOf([1, 2, 2, 2, 3], 2));
+console.log(_.sortedLastIndexOf([1, 2, 2, 2, 3], 3));
+
+//* Concat
+console.log(_.concat([], 1, 2, 3, 4));
+console.log(_.concat([0, 1], 2, 3, 4));
+
+//* Join
+console.log(_.join([1, 2, 3, 4, 5])); // 1,2,3,4,5
+console.log(_.join([1, 2, 3, 4, 5], "-")); // 1-2-3-4-5
+
+//* Drop and DropRight
+console.log(_.drop([1, 2, 3, 4, 5])); // [ 2, 3, 4, 5 ]
+console.log(_.drop([1, 2, 2, 3, 4, 5], 2)); // [ 2, 3, 4, 5 ]
+console.log(_.dropRight([1, 2, 3, 4, 5])); // [ 1, 2, 3, 4 ]
+console.log(_.dropRight([1, 2, 2, 3, 4, 5], 2)); // [ 1, 2, 2, 3 ]
+
+//* DropWhile and DropRightWhile
+const users = [
+  { user: "barney", age: 36, active: false },
+  { user: "fred", age: 40, active: false },
+  { user: "pebbles", age: 1, active: true },
+];
+console.log(
+  _.dropWhile(users, function (o) {
+    return o.active !== true;
+  })
+); // { user: 'pebbles', age: 1, active: true } ]
+console.log(
+  _.dropRightWhile(users, function (o) {
+    return o.active !== false;
+  })
+);
+// [
+//   { user: 'barney', age: 36, active: false },
+//   { user: 'fred', age: 40, active: false }
+// ]
+
+//* Fill
+console.log(_.fill([1, 2, 3, 4, 5], "*")); // [ '*', '*', '*', '*', '*' ]
+console.log(_.fill([1, 2, 3, 4, 5], "a")); // [ 'a', 'a', 'a', 'a', 'a' ]
+
+//- Intersection
+console.log(_.intersection([1, 2, 3, 4, 5], [3, 4, 5])); // [ 3, 4, 5 ]
+
+//* IntersectionBy
+console.log(_.intersectionBy([2, 4, 5, 6], [4, 8, 10], (i) => i / 2)); // [ 4 ]
+
+{
+  //* IntersectionWith
+  const obj1 = [
+    { a: 1, b: 2 },
+    { a: 2, b: 3 },
+  ];
+  const obj2 = [
+    { a: 1, b: 1 },
+    { a: 1, b: 2 },
+  ];
+
+  console.log(_.intersectionWith(obj1, obj2, _.isEqual)); // [ { a: 1, b: 2 } ]
+}
