313589807d375b24023130ce872c58ce4d758264
Michal Radomski <michalra@op.pl>
2025-06-13 22:01:31 +0200

    Lodash course - commit 06

--- 12 Lodash/01/02_arrays.ts
+++ 12 Lodash/01/02_arrays.ts
@@ -108,3 +108,110 @@ console.log(_.intersectionBy([2, 4, 5, 6], [4, 8, 10], (i) => i / 2)); // [ 4 ]
 
   console.log(_.intersectionWith(obj1, obj2, _.isEqual)); // [ { a: 1, b: 2 } ]
 }
+
+//* Pull and PullAll
+const array = [1, 2, 3];
+console.log(_.pull(array, 1)); // [ 2, 3 ]
+
+const array3 = [1, 2, 3, 4, 5];
+console.log(_.pullAll(array3, [1, 2])); // [ 3, 4, 5 ]
+
+//* PullAllBy
+{
+  const array = [{ a: 1 }, { a: 2 }, { a: 3 }];
+  console.log(_.pullAllBy(array, [{ a: 3 }], "a"));
+  console.log(_.pullAllBy(array, [{ a: 1 }, { a: 2 }], "a"));
+}
+
+{
+  //* PullAllWith
+  const array = [
+    { a: 1, b: 2 },
+    { a: 3, b: 4 },
+    { a: 5, b: 6 },
+  ];
+  console.log(_.pullAllWith(array, [{ a: 3, b: 4 }], _.isEqual));
+}
+
+{
+  //* PullAt
+  console.log(_.pullAt([1, 2, 3, 4], [1, 2]));
+
+  const objects3 = [
+    { year: 2016, month: 3 },
+    { year: 2014, month: 4 },
+    { year: 2012, month: 1 },
+    { year: 2019, month: 8 },
+    { year: 2012, month: 2 },
+    { year: 2019, month: 2 },
+    { year: 2016, month: 3 },
+    { year: 2016, month: 3 },
+  ];
+
+  console.log(_.pullAt(objects3, [2, 5, 7]));
+}
+
+{
+  //* Uniq and UniqBy
+  console.log(_.uniq([1, 2, 2, 2, 3, 3, 4, 5])); // [ 1, 2, 3, 4, 5 ]
+
+  console.log(_.uniq([1, 22, 22, 23, 34, 34, 40, 50]));
+  console.log(_.uniq(["a", "a", "a", "b", "b"]));
+
+  const objects3 = [
+    { year: 2016, month: 3 },
+    { year: 2014, month: 4 },
+    { year: 2012, month: 1 },
+    { year: 2019, month: 8 },
+    { year: 2012, month: 2 },
+    { year: 2019, month: 2 },
+    { year: 2016, month: 3 },
+    { year: 2016, month: 3 },
+  ];
+
+  console.log(_.uniqBy(objects3, (x) => x.month));
+  console.log(_.uniqBy(objects3, (x) => x.year));
+}
+
+{
+  //* UniqWith
+  const arr1 = [
+    { a: 1, b: 2 },
+    { a: 2, b: 1 },
+  ];
+
+  console.log(_.uniqWith(arr1, _.isEqual));
+}
+
+{
+  //* Union
+  console.log(_.union([1, 2, 2, 3]));
+  console.log(_.union([1, 2, 2, 3], [2, 2, 23, 4]));
+  console.log(_.union([1, 2, 2, 2, 3, 3, 4, 4, 5], [2, 2, 3, 4, 6, 7, 8], [4, 7, 8, 10, 12]));
+}
+
+//* UnionBy
+console.log(_.unionBy([1.2, 1.3, 1.4], [2.1, 2.2, 2.3], Math.floor));
+
+{
+  //* UnionWith
+  const arr1 = [
+    { a: 1, b: 2 },
+    { a: 2, b: 1 },
+  ];
+  const arr2 = [
+    { a: 1, b: 1 },
+    { a: 1, b: 2 },
+  ];
+  console.log(_.unionWith(arr1, arr2, _.isEqual)); // [ { a: 1, b: 2 }, { a: 2, b: 1 }, { a: 1, b: 1 } ]
+}
+
+//* First/Head and Last and Nth
+console.log(_.first([1, 2, 3, 4, 5])); // 1
+console.log(_.head([1, 2, 3, 4, 5])); // 1
+console.log(_.last([1, 2, 3, 4, 5])); // 5
+console.log(_.nth([1, 2, 3, 4, 5], 3)); // 4
+
+//* Reverse
+const numbers = [1, 2, 3, 4, 5];
+console.log(_.reverse(numbers));
