866e2255506c973f55266c9d3ed1993e2ef8befc
Michal Radomski <michalra@op.pl>
2025-06-15 20:14:52 +0200

    Lodash course - commit 23

--- 12 Lodash/01/07_functions.ts
+++ 12 Lodash/01/07_functions.ts
@@ -141,3 +141,64 @@ console.log(_.mapKeys({ a: 1, b: 2 }, (value, key) => key + value)); // { a1: 1,
 
   console.log(_.omit(object, ["a", "b"])); // { c: 3 }
 }
+
+{
+  //* OmitBy
+  const object = { a: 1, b: 2, c: 3 };
+  console.log(_.omit(object, "a")); // { b: 2, c: 3 }
+
+  console.log(_.omitBy(object, _.isNumber)); // {}
+  console.log(_.omitBy(object, !_.isNumber)); // { a: 1, b: 2, c: 3 }
+}
+
+{
+  //* FindKey
+  const users = [
+    { user: "barney", age: 36, active: false },
+    { user: "fred", age: 40, active: false },
+    { user: "pebbles", age: 1, active: true },
+  ];
+
+  console.log(_.find(users, (i) => i.age < 40)); // { user: 'barney', age: 36, active: false }
+
+  console.log(_.findKey(users, (i: { user: string; age: number; active: boolean }) => i.age === 40)); // 1
+}
+
+{
+  //* Defaults and Defaults Deep
+  const myobject = { a: 1, b: undefined };
+  console.log(_.defaults(myobject, { a: 2, b: 3 })); // { a: 1, b: 3 }
+
+  const myobject1 = { a: 1, b: { c: { d: undefined } } };
+  console.log(_.defaultsDeep(myobject1, { b: { c: { d: 2 } } })); // { a: 1, b: { c: { d: 2 } } }
+}
+
+{
+  //* Invoke
+  const object = { a: [{ b: { c: [1, 2, 3, 4] } }] };
+
+  console.log(_.invoke(object, "a[0].b.c.slice", 1, 3)); // [ 2, 3 ]
+}
+
+{
+  //* Result
+  const myobject = { a: _.constant(1), b: { c: 2, d: 3 }, f: () => 5 };
+  console.log(_.result(myobject, "a")); // 1
+}
+
+{
+  //* Pick and PickBy
+  const myobject = { a: 1, b: 2, c: 3 };
+
+  console.log(_.pick(myobject, ["a", "b"])); // { a: 1, b: 2 }
+  console.log(_.pick(myobject, ["c"])); // { c: 3 }
+  console.log(_.pickBy(myobject, _.isNumber)); // { a: 1, b: 2, c: 3 }
+}
+
+{
+  //* ForOwn and ForOwnRight
+  const myobject = { a: 1, b: 2, c: 3 };
+
+  console.log(_.forOwn(myobject, (value, key) => console.log(key, value))); // c 3 b 2 a 1
+  console.log(_.forOwnRight(myobject, (value, key) => console.log(key, value))); // { a: 1, b: 2, c: 3 }
+}
