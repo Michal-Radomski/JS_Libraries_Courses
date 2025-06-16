0132dcaaab65834f65bb5cfed7eed2abd60a8627
Michal Radomski <michalra@op.pl>
2025-06-16 23:15:11 +0200

    Lodash course - commit 27 - course done

--- 12 Lodash/01/09_other.ts
+++ 12 Lodash/01/09_other.ts
@@ -1,4 +1,5 @@
 import _ from "lodash";
+import _s from "underscore.string";
 
 //+ Sequencing and Chaining
 
@@ -31,3 +32,21 @@ console.log(
   console.log(_.at(object, ["a"])); // [ 1 ]
   console.log(_.at(object, ["a", "b"])); // [ 1, { c: [ 2, 3 ] } ]
 }
+
+//+ Underscore.String
+
+//- Formatting Numbers
+console.log(_s.numberFormat(2000, 2));
+console.log(_s.numberFormat(2234.234234, 2));
+
+//* Comparing Strings - Levenshtein
+console.log(_s.levenshtein("kitten", "sitten")); // 1
+console.log(_s.levenshtein("kitten", "apple")); // 5
+
+//* Counting - Count
+console.log(_s.count("hello world", "l"));
+console.log(_s.count("banana", "an"));
+
+//* Slugify
+console.log(_s.slugify("hello!WorlD  _:2323++")); // hello-world-2323
+console.log(_s.slugify("hello!World)  dasd %3455£££++Déjà vu")); // hello-world-dasd-3455-deja-vu
